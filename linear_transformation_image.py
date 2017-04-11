20ab81c359182d74857a6b95f17fbdf7817569e3
EduardoVaca <vacalalo@gmail.com>
2017-04-11 18:09:54 -0500

    Add method to print system of equations in formal way

--- linear_transformation_image.py
+++ linear_transformation_image.py
@@ -1,15 +1,14 @@
 """
 Author: Eduardo Vaca
 """
-
+variable_names = ['x', 'y', 'z']
 
 def build_system_of_equations():
 	"""Buids the equations system by user input.
 	The system is represented by a list of lists
 	This is only for 3 * 3 matrix (equations/variables)
 	"""
-	equations = variables = 3
-	variable_names = ['x', 'y', 'z']
+	equations = variables = 3	
 	system_of_equations = [[] for _ in range(equations)]	
 
 	for i in range(equations):
@@ -21,10 +20,22 @@ def build_system_of_equations():
 	return system_of_equations
 
 
+def print_system_of_equations(system_of_equations):
+	"""Prints the system of equations in formal way.
+	Params:
+		- system_of_equations: Matrix representing the system.
+	"""
+	print('\nYour System of Equations:')
+	for equation in system_of_equations:
+		print('|', end=' ')
+		for i in range(len(equation)):
+			print('{}{}'.format(equation[i], variable_names[i]), end=' ')
+		print('|')
+
 
 def main():
 	equations = build_system_of_equations()
-	print(equations)
+	print_system_of_equations(equations)
 
 if __name__ == '__main__':
 	main()
